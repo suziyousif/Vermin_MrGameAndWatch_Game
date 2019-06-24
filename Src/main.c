d723ba13e6d6c93efe58a5d218622f51bb599f58
Suzi Yousif <37808179+suziyousif@users.noreply.github.com>
2019-06-24 19:15:57 -0300

    Music - Buzzer

--- Src/main.c
+++ Src/main.c
@@ -9,12 +9,13 @@
 #include "NOKIA5110_fb.h"
 #include "figuras.h"
 #include "PRNG_LFSR.h"
+#include "string.h"
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
-
+TIM_HandleTypeDef htim3;
 //osThreadId defaultTaskHandle;
 
 /* USER CODE BEGIN PV */
@@ -29,39 +30,53 @@ typedef struct moles{
 	pontos_t Top_p;
 	pontos_t Middle_p;
 	pontos_t Bottom_p;
+	uint8_t top_flag;
 }moles_t;
 
 volatile uint32_t ADC_VALOR[2];
 uint32_t adcDmaBuffer[2];
-
-
 static pontos_t Mr_p;
 static pontos_t missing_p;
-
 static uint32_t j[5];  //a pos de cada mole gerado (são 5 posições possiveis)
 static uint32_t mole_num= 0;  //qtd de moles por rodada
 static uint32_t total_mole_n= 0;  //qtd total de moles
 static uint32_t last_missing = 0;
 static uint32_t media_eixoX, media_eixoY;
-
 xTaskHandle vTask_LCD_Print_Handle;
 
 player_t player;
 moles_t mole[5];
+
+uint32_t melody[3] = {
+		F1, E1, C1
+};
+
+uint32_t notes_duration[3]={
+		4, 4, 2
+};
+
+uint32_t nota_freq;
+char *nome_nota;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_TIM3_Init(void);
 
 /* USER CODE BEGIN PFP */
 void setMolesPos();
 void Start_Game();
 void create_tasks();
 void restart_game();
+void CLR_moleFlag();
+void ball_Hit_Sound();
+void game_over_song();
 void vTask_Game_Over(void *pvParameters);
+
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE END PFP */
 
@@ -81,29 +96,41 @@ void vTask_LCD_Print(void *pvParameters)
 {
 	while(1) imprime_LCD();
 }
-//---------------------------------------------------------------------------------------------------
-// Tarefa para imprimir um numero aleatorio
+
 void vTask_CreateMoles(void *pvParameters)
 {
-	uint8_t i;
+	uint8_t i,k;
 	while(1)
 	{
 		mole_num = (prng_LFSR() % 5) + 1;  //quantidade de moles por rodada 1 a 5
 		total_mole_n += mole_num;
-		for(i = 0; i<mole_num; i++){
-			j[i] = (prng_LFSR() % 4);  //sortear a pos de cada mole e armazenar num vetor 0 a 4
+		memset(j, 0, sizeof(j));  //zerar os valores anteriores
+		CLR_moleFlag();
+		for(i = 0; i < mole_num; i++){
+			j[i] = (prng_LFSR() % 5);  //sortear a pos de cada mole e armazenar num vetor 0 a 4
+			k=0;
+			while(k < (mole_num-1)){  //para não sortear a mesma posição
+				if((j[i] == j[k]) && (i!=k)){
+					j[i] = (prng_LFSR() % 4);
+					k=0;
+				}else
+					k++;
+			}
 			desenha_fig(&mole[j[i]].Bottom_p, &Bottom_mole);
-			vTaskDelay(MS(500));
+			vTaskDelay(MS(600));
 			desenha_fig(&mole[j[i]].Middle_p, &Middle_mole);
-			vTaskDelay(MS(500));
+			vTaskDelay(MS(600));
 			desenha_fig(&mole[j[i]].Top_p, &Top_mole);
-			vTaskDelay(MS(500));
+			mole[j[i]].top_flag = 1;
+			vTaskDelay(MS(300));
 		}
-		vTaskDelay(MS(500));
+		vTaskDelay(MS(800));
 		for(i = 0; i<mole_num; i++){  //apagar os moles gerados anteriormente
+			mole[j[i]].top_flag = 0;
 			desenha_fig(&mole[j[i]].Top_p, &delete_Top_mole);
 			desenha_fig(&mole[j[i]].Middle_p, &delete_Middle_mole);
 			desenha_fig(&mole[j[i]].Bottom_p, &delete_Bottom_mole);
+			vTaskDelay(MS(200));
 		}
 	}
 }
@@ -115,25 +142,31 @@ void vTask_checkScores(void *pvParameters){
 	while(1){
 		if(player.smash){
 			for(uint8_t i = 0; i < mole_num; i++){
-				if((Mr_p.x1) >= mole[i].Top_p.x1  && (Mr_p.x1) <= ((mole[i].Top_p.x1)+10)){
-					smash_p.x1 = mole[i].Top_p.x1;
-					desenha_fig(&smash_p, &smash);
-					vTaskDelay(MS(60));
-					desenha_fig(&smash_p, &delete_smash);
-					escreve_Nr_Peq(40,42,player.scores++, 3);
-					desenha_fig(&mole[i].Top_p, &delete_Top_mole);
-					desenha_fig(&mole[i].Middle_p, &delete_Middle_mole);
-					desenha_fig(&mole[i].Bottom_p, &delete_Bottom_mole);
-				}
-				else if ((Mr_p.x1+29) >= mole[i].Top_p.x1  && (Mr_p.x1+29) <= ((mole[i].Top_p.x1)+10)) {
-					smash_p.x1 = mole[i].Top_p.x1;
-					desenha_fig(&smash_p, &smash);
-					vTaskDelay(MS(60));
-					desenha_fig(&smash_p, &delete_smash);
-					escreve_Nr_Peq(40,42,player.scores++, 3);
-					desenha_fig(&mole[i].Top_p, &delete_Top_mole);
-					desenha_fig(&mole[i].Middle_p, &delete_Middle_mole);
-					desenha_fig(&mole[i].Bottom_p, &delete_Bottom_mole);
+				if(mole[j[i]].top_flag == 1){
+					if((Mr_p.x1) >= mole[j[i]].Top_p.x1  && (Mr_p.x1) <= ((mole[j[i]].Top_p.x1)+10)){   //martelo esquerdo
+						smash_p.x1 = mole[j[i]].Top_p.x1;
+						ball_Hit_Sound();
+						desenha_fig(&smash_p, &smash);
+						vTaskDelay(MS(80));
+						desenha_fig(&smash_p, &delete_smash);
+						escreve_Nr_Peq(40,42,player.scores++, 3);
+						mole[j[i]].top_flag = 0;
+						desenha_fig(&mole[j[i]].Top_p, &delete_Top_mole);
+						desenha_fig(&mole[j[i]].Middle_p, &delete_Middle_mole);
+						desenha_fig(&mole[j[i]].Bottom_p, &delete_Bottom_mole);
+					}
+					else if ((Mr_p.x1+29) >= mole[j[i]].Top_p.x1  && (Mr_p.x1+29) <= ((mole[j[i]].Top_p.x1)+10)) {  //martelo direito
+						smash_p.x1 = mole[j[i]].Top_p.x1;
+						ball_Hit_Sound();
+						desenha_fig(&smash_p, &smash);
+						vTaskDelay(MS(80));
+						desenha_fig(&smash_p, &delete_smash);
+						escreve_Nr_Peq(40,42,player.scores++, 3);
+						mole[j[i]].top_flag = 0;
+						desenha_fig(&mole[j[i]].Top_p, &delete_Top_mole);
+						desenha_fig(&mole[j[i]].Middle_p, &delete_Middle_mole);
+						desenha_fig(&mole[j[i]].Bottom_p, &delete_Bottom_mole);
+					}
 				}
 			}
 		}
@@ -141,7 +174,7 @@ void vTask_checkScores(void *pvParameters){
 	}
 }
 
-void vTask_missings_update(void *pvParameters){
+void vTask_LifeBar_update(void *pvParameters){
 	uint32_t x=0;
 
 	while(1){
@@ -152,24 +185,25 @@ void vTask_missings_update(void *pvParameters){
 				x = player.missing - last_missing;
 				last_missing = player.missing;
 				for(uint8_t k = 0; (k < x) && (missing_p.x1 != 0) ; k++){
-					missing_p.x1--;
 					desenha_fig(&missing_p, &life_line);
+					missing_p.x1--;
+					if(missing_p.x1 == 0){
+						xTaskCreate(vTask_Game_Over, "Task 8", 100, NULL, 2, NULL);
+						vTaskDelay(MS(1000));
+					}
 					vTaskDelay(MS(20));
 				}
 			}
-			if(missing_p.x1 == 0){
-				xTaskCreate(vTask_Game_Over, "Task 7", 100, NULL, 2, NULL);
-				vTaskDelay(MS(1000));
-			}
 		}
-
 	}
 }
 
 void vTask_Game_Over(void *pvParameters){
 	char buffer[2];
 	while(1){
+
 		vTaskPrioritySet(vTask_LCD_Print_Handle,2);		//coloca a prioridade de atualizar o LCD no mesmo nível desta tarefa
+
 		limpa_LCD();
 		goto_XY(15, 1);
 		string_LCD("Game-Over");
@@ -177,11 +211,13 @@ void vTask_Game_Over(void *pvParameters){
 		goto_XY(20, 2);
 		string_LCD("Scores");
 
-		goto_XY(20, 3);
+		goto_XY(32, 3);
 		itoa(player.scores, buffer, 10);
 		string_LCD(buffer);
 
-		vTaskDelay(MS(4000));
+		game_over_song();
+
+		vTaskDelay(MS(3000));
 
 		// Da opção de novo jogo
 		limpa_LCD();
@@ -259,6 +295,7 @@ void vTask_read_joystick(void *pvParameters)
 		media_eixoY = y_aux/32;
 	}
 }
+
 //---------------------------------------------------------------------------------------------------
 /* USER CODE END 0 */
 int main(void)
@@ -275,11 +312,14 @@ int main(void)
 	MX_GPIO_Init();
 	MX_DMA_Init();
 	MX_ADC1_Init();
-
+	MX_TIM3_Init();
 	/* USER CODE BEGIN 2 */
 	HAL_ADCEx_Calibration_Start(&hadc1);
 	HAL_ADC_Start_DMA(&hadc1,(uint32_t*)adcDmaBuffer,2);
 	HAL_ADC_Start_IT(&hadc1);
+
+	__HAL_TIM_SET_COMPARE(&htim3,TIM_CHANNEL_3, 100); // som
+
 
 	// inicializa LCD 5110
 	inic_LCD();
@@ -397,6 +437,55 @@ static void MX_ADC1_Init(void)
 
 }
 
+/* TIM3 init function */
+static void MX_TIM3_Init(void)
+{
+
+  TIM_ClockConfigTypeDef sClockSourceConfig;
+  TIM_MasterConfigTypeDef sMasterConfig;
+  TIM_OC_InitTypeDef sConfigOC;
+
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 4000;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 200;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  HAL_TIM_MspPostInit(&htim3);
+
+}
 /** 
   * Enable DMA controller clock
   */
@@ -428,6 +517,8 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
@@ -481,11 +572,18 @@ void setMolesPos(){
 	mole[4].Top_p.x1 = 76;      mole[4].Top_p.y1 = 24;
 }
 
+void CLR_moleFlag(){
+	mole[0].top_flag = 0;
+	mole[1].top_flag = 0;
+	mole[2].top_flag = 0;
+	mole[3].top_flag = 0;
+	mole[4].top_flag = 0;
+}
 void Start_Game(){
 	pontos_t p_line;
 
 	setMolesPos();
-
+	CLR_moleFlag();
     player.scores = 0;
     player.smash = 0;
     player.missing = 0;
@@ -526,7 +624,7 @@ void restart_game(){
 	player.scores = 0;
 	player.smash = 0;
 	player.missing = 0;
-
+	CLR_moleFlag();
 	total_mole_n = 0;
 	last_missing = 0;
 
@@ -545,13 +643,34 @@ void restart_game(){
 
 	escreve_Nr_Peq(40,42,player.scores, 3);
 }
+
+void ball_Hit_Sound()
+{
+	__HAL_TIM_SET_PRESCALER(&htim3,36000/32);
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+	vTaskDelay(MS(100));
+	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3);
+}
+
+void game_over_song(){
+	for(uint8_t i=0; i<3; i++ ){
+		HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3);
+		vTaskDelay(MS(10));
+		__HAL_TIM_SET_PRESCALER(&htim3,(36000/melody[i])-1);
+		HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+		vTaskDelay(MS(2000/notes_duration[i]));
+	}
+	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3);
+	vTaskDelay(MS(500));
+}
+
 void create_tasks(){
 	xTaskCreate(vTask_LCD_Print, "Task 1", 100, NULL, 1,&vTask_LCD_Print_Handle);
 	xTaskCreate(vTask_checkScores, "Task 2", 100, NULL, 1,NULL);
 	xTaskCreate(vTask_read_joystick, "Task 3", 100, NULL, 1, NULL);
 	xTaskCreate(vTask_CreateMoles, "Task 4", 100, NULL, 1,NULL);
 	xTaskCreate(vTask_move_MrWatchAndGame, "Task 5", 100, NULL, 1,NULL);
-	xTaskCreate(vTask_missings_update, "Task 6", 100, NULL, 1,NULL);
+	xTaskCreate(vTask_LifeBar_update, "Task 6", 100, NULL, 1,NULL);
 }
 /* USER CODE END 4 */
 
--- Src/stm32f1xx_hal_msp.c
+++ Src/stm32f1xx_hal_msp.c
@@ -53,7 +53,7 @@ extern DMA_HandleTypeDef hdma_adc1;
 
 extern void _Error_Handler(char *, int);
 /* USER CODE BEGIN 0 */
-
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
 /* USER CODE END 0 */
 /**
   * Initializes the Global MSP.
@@ -169,6 +169,64 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
 
 }
 
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
+{
+
+  if(htim_base->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspInit 0 */
+
+  /* USER CODE END TIM3_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM3_CLK_ENABLE();
+  /* USER CODE BEGIN TIM3_MspInit 1 */
+
+  /* USER CODE END TIM3_MspInit 1 */
+  }
+
+}
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  if(htim->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspPostInit 0 */
+
+  /* USER CODE END TIM3_MspPostInit 0 */
+
+    /**TIM3 GPIO Configuration
+    PB0     ------> TIM3_CH3
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN TIM3_MspPostInit 1 */
+
+  /* USER CODE END TIM3_MspPostInit 1 */
+  }
+
+}
+
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
+{
+
+  if(htim_base->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspDeInit 0 */
+
+  /* USER CODE END TIM3_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM3_CLK_DISABLE();
+  /* USER CODE BEGIN TIM3_MspDeInit 1 */
+
+  /* USER CODE END TIM3_MspDeInit 1 */
+  }
+
+}
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
